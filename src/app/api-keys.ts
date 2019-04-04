f9be270dd0cca33c28ec245b14381f48972bb077
Nayomi Morita and Nelia Terrazas <nayomi+nelia@>
2019-04-04 13:14:13 -0700

    day 2

---
+++ src/app/api-keys.ts
@@ -0,0 +1,8 @@
+export const masterFirebaseConfig = {
+  apiKey: "AIzaSyChTGNRFGUAH6Jj85YwcaBwAnaMlT73mFM",
+  authDomain: "online-marketplace-dbc21.firebaseapp.com",
+  databaseURL: "https://online-marketplace-dbc21.firebaseio.com",
+  projectId: "online-marketplace-dbc21",
+  storageBucket: "online-marketplace-dbc21.appspot.com",
+  messagingSenderId: "1045923591288"
+  };
